f84b95f859a667de46bf10cf0a4b7b7c365e272d
Ishita Singh <ishitasingh.2315@gmail.com>
2026-08-08 16:16:44 +0530

    Added UC1 Print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPS Banner App - Use Case 1
+ *
+ * @author Ishita Singh
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
