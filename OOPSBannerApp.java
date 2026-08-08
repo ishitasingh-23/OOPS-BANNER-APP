4d25a2cae49b8beaf0e8ba8d5867ee754576fdfe
Ishita Singh <ishitasingh.2315@gmail.com>
2026-08-08 16:49:44 +0530

    Added UC6 Static Functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,61 @@
 /**
- * OOPS Banner App - Use Case 5
+ * OOPS Banner App - Use Case 6
  *
  * @author Ishita Singh
- * @version 5.0
+ * @version 6.0
  */
 public class OOPSBannerApp {
 
+    static String[] getOPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    static String[] getPPattern() {
+        return new String[]{
+            "***** ",
+            "*    *",
+            "*    *",
+            "***** ",
+            "*     ",
+            "*     ",
+            "*     "
+        };
+    }
+
+    static String[] getSPattern() {
+        return new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
+    }
+
     public static void main(String[] args) {
 
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
         String[] banner = {
-            String.join("", " ***** ", " **** ", " **** ", " ***** "),
-            String.join("", " *     ", "* *   * ", "* *   * ", "*     "),
-            String.join("", " *     ", "* *   * ", "* *   * ", "*     "),
-            String.join("", " ***** ", " ****  ", "*   *  ", " ***** "),
-            String.join("", "      *", " *     ", "*   *  ", "    * "),
-            String.join("", "      *", " *     ", "*   *  ", "    * "),
-            String.join("", " ***** ", " ***   ", "*   *  ", " ***** ")
+            String.join("  ", o[0], p[0], p[0], s[0]),
+            String.join("  ", o[1], p[1], p[1], s[1]),
+            String.join("  ", o[2], p[2], p[2], s[2]),
+            String.join("  ", o[3], p[3], p[3], s[3]),
+            String.join("  ", o[4], p[4], p[4], s[4]),
+            String.join("  ", o[5], p[5], p[5], s[5]),
+            String.join("  ", o[6], p[6], p[6], s[6])
         };
 
         for (String line : banner) {
